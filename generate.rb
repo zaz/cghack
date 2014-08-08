9dbca6055cb4095c0f0598338ba95959d7103cdf
Josh Brown <joshbrown15@gmail.com>
2014-08-08 13:32:16 +0000

    Add generator
    Add a script to leverage the power of the GitHub contributions graph to
    calculate the answer to life, the universe, and everything.

---
+++ generate.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env ruby
+
+exit unless system "git branch -D hack; git checkout --orphan hack"
+system "git rm -rf --cached ."
+
+DAY = 60 * 60 * 24
+
+# Pixel darkness in hexadecimal. Try to keep the total value low (< ~1000).
+IMAGE="\
+     777        77               7 7   77          
+    7          7  7              7 7  7  7         
+    777   7 7  7  7  7777 7777  7  7     7         
+    7  7   7    777             77777   7          
+    7  7  7 7     7  7777 7777     7   7           
+     77        777                 7  7777         
+                                                   "
+
+image = IMAGE.split("\n").map { |r| r.split('') }
+image = image.transpose.flatten.map { |d| d.to_i(16) }
+
+day, month, year = Time.now.to_a[3..5]
+time = Time.new(year - 1, month, day, 12)
+time = time + (7 - time.wday) * DAY
+
+for value in image
+	value.times do
+		system "git commit --allow-empty --allow-empty-message -m '' --date='#{time}' >/dev/null"
+	end
+	time = time + DAY
+end
