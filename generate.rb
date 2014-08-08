c2477d535f9141dc01180e4b6c81b0f5d71644b0
Josh Brown <joshbrown15@gmail.com>
2014-08-08 15:06:21 +0000

    Add runtime info about total number of commits

--- generate.rb
+++ generate.rb
@@ -22,6 +22,10 @@ day, month, year = Time.now.to_a[3..5]
 time = Time.new(year - 1, month, day, 12)
 time = time + (7 - time.wday) * DAY
 
+commits = image.reduce(:+)
+puts "WARNING: #{commits} is a lot of commits." if commits > 999
+puts "Making #{commits} commits..."
+
 for value in image
 	value.times do
 		system "git commit --allow-empty --allow-empty-message -m '' --date='#{time}' >/dev/null"
